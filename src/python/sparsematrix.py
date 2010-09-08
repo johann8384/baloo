28b8594615861d0da57c504bc2eb1c6c551f22c5
Xavier <xavier.stevens@gmail.com>
2010-09-08 11:06:43 -0700

    first commit

---
+++ src/python/sparsematrix.py
@@ -0,0 +1,29 @@
+from numpy import zeros, array
+
+def read_sparse_matrix(srcfile, msize):
+    fin = open(srcfile, "r")
+    m = []
+    for line in fin:
+        splits = line.split('\t')
+        r = zeros(msize+1)
+        for j in range(1, len(splits)):
+            splits[j] = splits[j].strip()
+            if len(splits[j]) > 0:
+                r[int(splits[j])] = 1
+    
+        m.append(r[1:])
+
+    fin.close()
+    return array(m)
+    
+def read_feature_index(srcfile):
+    feature_index = []
+    
+    fin = open(srcfile, "r")
+    for line in fin:
+        splits = line.split('\t')
+        feature = splits[1]
+        count = int(splits[2])
+        feature_index.append((feature, count))
+        
+    return feature_index
