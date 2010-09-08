5427daabe62318c6893443c7a3aa992ffb7b8198
Xavier <xavier.stevens@gmail.com>
2010-09-08 11:09:32 -0700

    getting used to git committing changes

--- src/python/cluster-analysis.py
+++ src/python/cluster-analysis.py
@@ -5,9 +5,9 @@ from scipy.cluster.vq import vq, kmeans, whiten
 from sparsematrix import read_sparse_matrix, read_feature_index
 import matplotlib.pyplot as plt
 
-feature_index = read_feature_index("/Users/xstevens/workspace/socorro/analysis/feature-index.txt")
+feature_index = read_feature_index("feature-index.txt")
 print "Feature Index: contains %d features" % (len(feature_index))
-x = read_sparse_matrix("/Users/xstevens/workspace/socorro/analysis/data-matrix.txt", len(feature_index))
+x = read_sparse_matrix("data-matrix.txt", len(feature_index))
 #y = pdist(x, 'euclidean')
 z = linkage(x, method='centroid')
 #fc = fcluster(z, t=100, criterion='maxclust')
